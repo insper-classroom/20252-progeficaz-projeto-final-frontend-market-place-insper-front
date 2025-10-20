1246438e085035cf47ab36274fe14fcdbd912f2e
Carolina Amorim <carolinaoa1@al.insper.edu.com>
2025-10-20 17:17:34 -0300

    ajuste na organização dos arquivos

--- src/pages/cadastro.js
+++
@@ -1,8 +0,0 @@
-export default function Cadastro() {
-  return (
-    <div style={{ textAlign: "center", marginTop: "40px" }}>
-      <h1>📝 Cadastro</h1>
-      <p>Crie sua conta e comece a usar o <strong>MarketInsper</strong>!</p>
-    </div>
-  );
-}
