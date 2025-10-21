1b931f4582c5db83a73860e82403984b9a04f85e
Gabrielli Araujo <gabisaraujo0707@gmail.com>
2025-10-21 08:39:29 -0300

    fix: Base projeto rodando

--- src/components/Homepage/home.jsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ textAlign: "center", marginTop: "40px" }}>
-      <h1>Página Inicial</h1>
-      <p>Bem-vindo ao <strong>MarketInsper</strong>! 🚀</p>
-    </div>
-  );
-}
