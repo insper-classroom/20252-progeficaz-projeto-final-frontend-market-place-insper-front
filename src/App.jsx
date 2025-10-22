10654400d6751dada61637985228aa76239882d9
Carolina Amorim <carolinaoa1@al.insper.edu.com>
2025-10-22 18:01:52 -0300

    pequeno ajuste após merge

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,6 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Footer from './components/Footer/footer'
-import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 import Home from "./components/Home/home";
 
 function App() {
