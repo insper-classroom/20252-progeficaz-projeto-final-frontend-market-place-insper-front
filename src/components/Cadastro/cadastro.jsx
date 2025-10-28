3425d32a1656c8ca6697323dcdc07b48f4877b39
Lara <lara.abduni@gmail.com>
2025-10-28 09:33:01 -0300

    tirei uma linha

--- src/components/Cadastro/cadastro.jsx
+++ src/components/Cadastro/cadastro.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState, useRef } from 'react';
-import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import authService from '../../services/authService';
